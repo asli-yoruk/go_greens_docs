cf5f02f1a40f9275529ce86cadfd41fda4411b7d
Asli Yoruk <asli-yoruk@hotmail.com>
2025-03-07 17:47:35 +1000

    changed title and description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Go Greens',
+  tagline: 'Make the world greener!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
